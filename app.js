83cc2628319387b0c42ddd87424bf2994c4c531e
noorkhadija557 <noorkhadija557@gmail.com>
2026-06-14 14:21:42 +0500

    done

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ let onBtn = document.getElementById("onBtn");
 let offBtn = document.getElementById("offBtn");
 
 onBtn.addEventListener("click", function () {
-    bulb.src = "images/on.avif";
+    bulb.src = "images/ON BULB.png";
 });
 
 offBtn.addEventListener("click", function () {
-    bulb.src = "images/bulb off1.avif";
+    bulb.src = "images/OFF BULB.png";
 });
